d7e9d6f95884b3407986470d350110e27c4120f2
Mike <mikeward5@hotmail.co.uk>
2026-06-04 09:01:08 +0100

    completed repeatString reverseString and removeFromArray

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,5 @@
-const repeatString = function() {
+const repeatString= (str) => str.repeat(3);
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...index) {
+    let newArr = [];
+    arr.forEach((item)=> { 
+        if (!index.includes(item)) {
+            newArr.push(item)
+        }
+    })
+    return newArr;
 };
 
 // Do not edit below this line
